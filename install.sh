ecff2254451634ec5765edd117df824ae9ce7f78
fred-penot <frederic.penot@gmail.com>
2016-12-04 09:34:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,9 @@
-cd /var/www/html
-git clone https://github.com/fred-penot/DomoApi.git
-mv /var/www/html/DomoApi /var/www/html/domoapi
-chmod -Rf 777 /var/www/html/domoapi
-cd /var/www/html/domoapi
-/usr/local/zend/bin/php composer.phar install
-rm -f /var/www/html/domoapi/vendor/knplabs/console-service-provider/Knp/Provider/ConsoleServiceProvider.php
-cp /var/www/html/domoapi/app/doc/ConsoleServiceProvider.php /var/www/html/domoapi/vendor/knplabs/console-service-provider/Knp/Provider/
-cp -Rf /var/www/html/domoapi/app/doc/allocine /var/www/html/domoapi/vendor/
-rm -f /var/www/html/domoapi/vendor/composer/autoload_psr4.php
-cp /var/www/html/domoapi/app/doc/autoload_psr4.php /var/www/html/domoapi/vendor/composer/
-rm -f /var/www/html/domoapi/vendor/phansys/getid3/GetId3/Lib/Helper.php
-cp /var/www/html/domoapi/app/doc/Helper.php /var/www/html/domoapi/vendor/phansys/getid3/GetId3/Lib/
-rm -f /var/www/html/domoapi/vendor/phansys/getid3/GetId3/GetId3.php
-cp /var/www/html/domoapi/app/doc/GetId3.php /var/www/html/domoapi/vendor/phansys/getid3/GetId3/
-cd /var/www/html/domoapi/app/doc
+login_ssh=docker
+
+cd /home/${login_ssh}
 unzip DomoApi.sql.zip
 unzip HomeOS.sql.zip
 rm -f DomoApi.sql.zip
 rm -f HomeOS.sql.zip
-chmod -Rf 777 /var/www/html/domoapi
+chmod -f 777 /home/${login_ssh}/DomoApi.sql
+chmod -f 777 /home/${login_ssh}/HomeOS.sql
